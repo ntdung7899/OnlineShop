fa9d6e7c8264453116a7e42da8e3d9a75ae32418
ntdung899 <ntdung899@gmail.com>
2020-03-14 16:41:02 +0700

    Add Template

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -8,22 +8,25 @@ namespace OnlineShop.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
+        public ActionResult News()
+        {
+            return View();
+        }
+        public ActionResult Produce()
+        {
+            return View();
+        }
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
